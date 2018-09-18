d81eda5f37902e04562ecbe067c8995f62024031
Aaron Bougouneau <a.bougouneau@gmail.com>
2018-09-18 08:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var recipes = {
 
 }
 
-/* function updateObjectWithKeyAndValue(object, key, value) {
-  object['key'] = value
-  return object
+function updateObjectWithKeyAndValue(object, key, value) {
+  return object.assign({}, object, { [key]:value})
 }
-*/
