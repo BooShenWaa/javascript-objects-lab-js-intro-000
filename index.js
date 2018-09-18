0a692e91f4bca2f4aa5abf75a4cdf02b5f2f28b7
Aaron Bougouneau <a.bougouneau@gmail.com>
2018-09-18 09:07:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj, key){
- cont obj2 = delete obj[key];
+  const obj2 = delete obj[key];
   return obj2
 }
 
